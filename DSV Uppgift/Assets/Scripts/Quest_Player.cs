9d1e7b29c200450ec11bacecc38af22d4ab7c41e
williamoscarsson <90610386+williamoscarsson@users.noreply.github.com>
2021-09-13 13:00:40 +0200

    Update Quest_Player.cs

--- DSV Uppgift/Assets/Scripts/Quest_Player.cs
+++ DSV Uppgift/Assets/Scripts/Quest_Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Quest_Player : MonoBehaviour
 {
     public bool isQuestComplete = false;
+    //Test 123 a
     // Start is called before the first frame update
     void Start()
     {
